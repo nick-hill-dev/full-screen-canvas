e4c6065f430b8b7c889149f42238cb00a0251e6c
hg <none@none>
2021-04-08 19:18:43 +0100

    Initial commit.

---
+++ src/FullScreenCanvas.ts
@@ -0,0 +1,38 @@
+class FullScreenCanvas {
+
+    public static add(parent: HTMLElement = document.body): HTMLCanvasElement {
+
+        let canvas = document.createElement('canvas');
+        parent.appendChild(canvas);
+
+        canvas.style.position = 'absolute';
+        canvas.style.overflow = 'hidden';
+        canvas.style.left = '0';
+        canvas.style.top = '0';
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        let resizeHandler = function () {
+            canvas.style.position = 'absolute';
+            canvas.style.overflow = 'hidden';
+            canvas.style.left = '0';
+            canvas.style.top = '0';
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+        };
+
+        resizeHandler();
+
+        window.addEventListener('resize', () => {
+            resizeHandler();
+        }, false);
+
+        return canvas;
+
+    }
+
+    public static remove(canvas: HTMLCanvasElement) {
+        canvas.parentElement.removeChild(canvas);
+    }
+
+}
