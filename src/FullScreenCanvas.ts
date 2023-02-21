8f66fc8ad08c37e0978aa6ce7f97013576f44069
Nicholas Hill <nick@nick-hill.com>
2023-02-21 14:16:57 +0000

    Improved management of the canvas elements and their associated event handlers.

--- src/FullScreenCanvas.ts
+++ src/FullScreenCanvas.ts
@@ -1,31 +1,44 @@
 class FullScreenCanvas {
 
+    private static canvases: Array<{
+        canvas: HTMLCanvasElement,
+        resizeHandler: (ev: UIEvent) => void;
+    }> = [];
+
     public static add(parent: HTMLElement = document.body): HTMLCanvasElement {
 
         let canvas = document.createElement('canvas');
         parent.appendChild(canvas);
 
-        let resizeHandler = function () {
-            canvas.style.position = 'absolute';
-            canvas.style.overflow = 'hidden';
-            canvas.style.left = '0';
-            canvas.style.top = '0';
-            canvas.width = window.innerWidth;
-            canvas.height = window.innerHeight;
+        FullScreenCanvas.resizeHandler(canvas);
+
+        let newManager = {
+            canvas: canvas,
+            resizeHandler: (ev: UIEvent) => FullScreenCanvas.resizeHandler(canvas)
         };
 
-        resizeHandler();
-
-        window.addEventListener('resize', () => {
-            resizeHandler();
-        }, false);
+        window.addEventListener('resize', newManager.resizeHandler, false);
 
         return canvas;
-
     }
 
     public static remove(canvas: HTMLCanvasElement) {
         canvas.parentElement.removeChild(canvas);
+        let manager = this.canvases.find(c => c.canvas === canvas);
+        if (manager) {
+            window.removeEventListener('resize', manager.resizeHandler, false);
+            let index = this.canvases.indexOf(manager);
+            this.canvases.splice(index, 1);
+        }
     }
+
+    private static resizeHandler(canvas: HTMLCanvasElement) {
+        canvas.style.position = 'absolute';
+        canvas.style.overflow = 'hidden';
+        canvas.style.left = '0';
+        canvas.style.top = '0';
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+    };
 
 }
